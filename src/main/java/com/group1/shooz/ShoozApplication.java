9762f534d39da84b80af4a5a9e001016a1adec59
Danish Roslan <danishroslan.work@gmail.com>
2024-05-14 13:14:27 +0800

    testing 1

--- src/main/java/com/group1/shooz/ShoozApplication.java
+++ src/main/java/com/group1/shooz/ShoozApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoozApplication {
 
 	public static void main(String[] args) {
-		//mnnn
+		//bark
 
 		SpringApplication.run(ShoozApplication.class, args);
 	}
