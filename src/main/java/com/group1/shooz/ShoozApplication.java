09798e81731d9fbe7bad9cad7f9f8dda65d7e249
maisa <maaidamia@gmail.com>
2024-06-12 13:23:02 +0800

    nothing

--- src/main/java/com/group1/shooz/ShoozApplication.java
+++ src/main/java/com/group1/shooz/ShoozApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoozApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoozApplication.class, args);
+		SpringApplication.run(ShoozApplication.class, args); //l
 	}
 
 }
