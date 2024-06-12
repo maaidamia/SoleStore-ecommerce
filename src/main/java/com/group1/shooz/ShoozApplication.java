d7d1c3312efb33ee66427ee4e7a30693b0ed21e8
maisa <maaidamia@gmail.com>
2024-06-12 13:23:58 +0800

    nothing

--- src/main/java/com/group1/shooz/ShoozApplication.java
+++ src/main/java/com/group1/shooz/ShoozApplication.java
@@ -9,7 +9,7 @@ public class ShoozApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoozApplication.class, args); //l
+		SpringApplication.run(ShoozApplication.class, args);
 	}
 
 }
